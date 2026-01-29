c899ed08cd811be0ef9154b35f83061410a88885
GabrielCWT <77312579+GabrielCWT@users.noreply.github.com>
2026-01-29 12:34:57 +0800

    Add mock for userModel.findById

--- models/__mocks__/userModel.js
+++ models/__mocks__/userModel.js
@@ -1,3 +1,7 @@
+export const NON_EXISTENT_USER_ID = 'nonExistentUserId';
+export const NON_ADMIN_USER_ID = 'nonAdminUserId';
+export const ADMIN_USER_ID = 'adminUserId';
+
 // Constructor
 const userModel = jest.fn(function (user) {
     Object.assign(this, user);
@@ -6,6 +10,18 @@ const userModel = jest.fn(function (user) {
 userModel.findOne = jest.fn();
 userModel.prototype.save = jest.fn(function () {
     return Promise.resolve(this);
+});
+userModel.findById = jest.fn((id) => {
+    switch (id) {
+        case NON_EXISTENT_USER_ID:
+            return null;
+        case NON_ADMIN_USER_ID:
+            return Promise.resolve({ _id: id, role: 0 });
+        case ADMIN_USER_ID:
+            return Promise.resolve({ _id: id, role: 1 });
+        default:
+            return null;
+    }
 });
 
 export default userModel;
