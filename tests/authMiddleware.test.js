e092d0c41192635ab834fb5d10df43e8d7c65595
GabrielCWT <77312579+GabrielCWT@users.noreply.github.com>
2026-02-06 18:37:52 +0800

    Add test for requireSignIn middleware

--- tests/authMiddleware.test.js
+++ tests/authMiddleware.test.js
@@ -1,13 +1,64 @@
 import { makeRes } from "../helpers/utils.test.js";
-import { isAdmin } from "../middlewares/authMiddleware";
+import { isAdmin, requireSignIn } from "../middlewares/authMiddleware";
 import {
   ADMIN_USER_ID,
   NON_ADMIN_USER_ID,
   NON_EXISTENT_USER_ID,
 } from "../models/__mocks__/userModel.js";
 import userModel from "../models/userModel.js";
+import JWT from "jsonwebtoken";
 
 jest.mock("../models/userModel.js");
+jest.mock("jsonwebtoken");
+
+describe("requireSignIn middleware", () => {
+  const res = makeRes();
+  const next = jest.fn();
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  test("valid token allows access", async () => {
+    const user = { _id: "validUserId" };
+    JWT.verify.mockReturnValue(user);
+    const req = {
+      headers: {
+        authorization: "validToken",
+      },
+    };
+
+    await requireSignIn(req, res, next);
+
+    expect(JWT.verify).toHaveBeenCalledWith(
+      "validToken",
+      process.env.JWT_SECRET
+    );
+    expect(req.user).toEqual(user);
+    expect(next).toHaveBeenCalled();
+  });
+
+  test("invalid token denies access", async () => {
+    JWT.verify.mockImplementation(() => {
+      throw new Error("Invalid token");
+    });
+    const req = {
+      headers: {
+        authorization: "invalidToken",
+      },
+    };
+    const consoleSpy = jest.spyOn(console, "log").mockImplementation();
+
+    await requireSignIn(req, res, next);
+
+    expect(JWT.verify).toHaveBeenCalledWith(
+      "invalidToken",
+      process.env.JWT_SECRET
+    );
+    expect(consoleSpy).toHaveBeenCalled();
+    expect(next).not.toHaveBeenCalled();
+  });
+});
 
 describe("isAdmin Middleware", () => {
   let req;
@@ -21,10 +72,12 @@ describe("isAdmin Middleware", () => {
   });
 
   test("malformed request returns 400 error", async () => {
-    req = {};
+    req = { body: {} };
+    const consoleSpy = jest.spyOn(console, "log").mockImplementation();
 
     await isAdmin(req, res, next);
 
+    expect(consoleSpy).toHaveBeenCalled();
     expect(res.status).toHaveBeenCalledWith(400);
     expect(res.send).toHaveBeenCalledWith(
       expect.objectContaining({
