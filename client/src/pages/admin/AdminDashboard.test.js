cd1759182ea1e7a1586917da0ede441d4cf33332
GabrielCWT <77312579+GabrielCWT@users.noreply.github.com>
2026-02-12 17:07:22 +0800

    Add AdminDashboard tests

---
+++ client/src/pages/admin/AdminDashboard.test.js
@@ -0,0 +1,61 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import { MemoryRouter } from "react-router-dom";
+import AdminDashboard from "./AdminDashboard";
+import Layout from "../../components/Layout";
+import AdminMenu from "../../components/AdminMenu";
+
+jest.mock("../../components/AdminMenu");
+
+jest.mock("../../components/Layout", () => {
+  return {
+    __esModule: true,
+    default: jest.fn(({ children }) => children),
+  };
+});
+
+const mockAuth = {
+  user: {
+    name: "Admin User",
+    email: "admin@gmail.com",
+    phone: "1234567890",
+  },
+};
+
+jest.mock("../../context/auth", () => ({
+  useAuth: jest.fn(() => [mockAuth, jest.fn()]),
+}));
+
+describe("AdminDashboard Component", () => {
+  test("renders layout", () => {
+    render(
+      <MemoryRouter>
+        <AdminDashboard />
+      </MemoryRouter>
+    );
+
+    expect(Layout).toBeCalled();
+  });
+
+  test("AdminMenu is rendered", () => {
+    render(
+      <MemoryRouter>
+        <AdminDashboard />
+      </MemoryRouter>
+    );
+
+    expect(AdminMenu).toBeCalled();
+  });
+
+  test("displays admin user information", () => {
+    const { getByText } = render(
+      <MemoryRouter>
+        <AdminDashboard />
+      </MemoryRouter>
+    );
+
+    expect(getByText(/Admin Name : Admin User/i)).toBeInTheDocument();
+    expect(getByText(/Admin Email : admin@gmail.com/i)).toBeInTheDocument();
+    expect(getByText(/Admin Contact : 1234567890/i)).toBeInTheDocument();
+  });
+});
