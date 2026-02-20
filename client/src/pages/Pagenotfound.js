f32de9f7f784fb318a7bb2c083f912341f742d46
C5hives <e1122945@u.nus.edu>
2026-02-20 13:45:38 +0800

    Add tests for Pagenotfound.js

--- client/src/pages/Pagenotfound.js
+++ client/src/pages/Pagenotfound.js
@@ -4,10 +4,10 @@ import Layout from "./../components/Layout";
 
 const Pagenotfound = () => {
   return (
-    <Layout title={"go back- page not found"}>
+    <Layout title={"go back - page not found"}>
       <div className="pnf">
         <h1 className="pnf-title">404</h1>
-        <h2 className="pnf-heading">Oops ! Page Not Found</h2>
+        <h2 className="pnf-heading">Oops! Page Not Found</h2>
         <Link to="/" className="pnf-btn">
           Go Back
         </Link>
