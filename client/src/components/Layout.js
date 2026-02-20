7f4056de2595b9bb505821af00d4bfbf734e5434
C5hives <e1122945@u.nus.edu>
2026-02-20 16:20:51 +0800

    Add tests for Layout.js

--- client/src/components/Layout.js
+++ client/src/components/Layout.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Footer from './Footer';
 import Header from './Header';
 import { Helmet } from "react-helmet";
-import  { Toaster } from 'react-hot-toast';
+import { Toaster } from 'react-hot-toast';
 
 const Layout = ({ children, title, description, keywords, author }) => {
   return (
