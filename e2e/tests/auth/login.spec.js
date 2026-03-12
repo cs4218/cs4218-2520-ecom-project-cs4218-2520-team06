ac3e6fc4c6a7d521088eb843007e1857b12f5311
GabrielCWT <77312579+GabrielCWT@users.noreply.github.com>
2026-03-12 16:07:17 +0800

    Add UI test for login

--- e2e/tests/auth/login.spec.js
+++ e2e/tests/auth/login.spec.js
@@ -1,1 +1,33 @@
-// placeholder test file for e2e tests, to be implemented
+import { describe, test, expect } from "@playwright/test";
+
+describe("Login Tests", () => {
+  test("Login with valid credentials", async ({ page }) => {
+    await page.goto("http://localhost:3000/login");
+    await page.getByRole("textbox", { name: "Enter Your Email" }).click();
+    await page
+      .getByRole("textbox", { name: "Enter Your Email" })
+      .fill("testRegister@gmail.com");
+    await page.getByRole("textbox", { name: "Enter Your Password" }).click();
+    await page
+      .getByRole("textbox", { name: "Enter Your Password" })
+      .fill("admin123");
+    await page.getByRole("button", { name: "LOGIN" }).click();
+    await expect(page.getByRole("main")).toContainText("🙏login successfully");
+  });
+
+  test("Login with invalid credentials", async ({ page }) => {
+    await page.goto("http://localhost:3000/login");
+    await page.getByRole("textbox", { name: "Enter Your Email" }).click();
+    await page
+      .getByRole("textbox", { name: "Enter Your Email" })
+      .fill("invalid@gmail.com");
+    await page.getByRole("textbox", { name: "Enter Your Password" }).click();
+    await page
+      .getByRole("textbox", { name: "Enter Your Password" })
+      .fill("invalid123");
+    await page.getByRole("button", { name: "LOGIN" }).click();
+    await expect(page.getByRole("main")).toContainText(
+      "Email is not registered"
+    );
+  });
+});
