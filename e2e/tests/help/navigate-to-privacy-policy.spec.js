d299b65721c18278462e05d1bc47ef61ac36f256
Kok Bo Chang <60499925+C5hives@users.noreply.github.com>
2026-03-21 11:18:48 +0000

    Add E2E tests for navigating to help pages (#5)
    * Add E2E tests for navigating to help pages
    
    * Add attribution

---
+++ e2e/tests/help/navigate-to-privacy-policy.spec.js
@@ -0,0 +1,60 @@
+// Kok Bo Chang, A0273542E
+import { test, expect } from '@playwright/test';
+
+test.beforeEach(async ({ page }) => {
+  await page.goto('http://localhost:3000/');
+})
+
+test('privacy policy page should be accessible from the home page when logged out', async ({ page }) => {
+  // Act
+  await page.getByRole('link', { name: 'Privacy Policy' }).click();
+
+  // Assert
+  await expect(page.getByRole('heading', { name: 'PRIVACY POLICY' })).toBeVisible();
+});
+
+test('privacy policy page should be accessible from the home page when logged in', async ({ page }) => {
+  // Arrange
+  const user = {
+    email: "cs4218@test.com",
+    password: "cs4218@test.com",
+  }
+
+  // Act
+  await page.getByRole('link', { name: 'Login' }).click();
+  await page.getByRole('textbox', { name: 'Enter Your Email' }).fill(user.email);
+  await page.getByRole('textbox', { name: 'Enter Your Password' }).fill(user.password);
+  await page.getByRole('button', { name: 'LOGIN' }).click();
+
+  await page.getByRole('link', { name: 'Privacy Policy' }).click();
+
+  // Assert
+  await expect(page.getByRole('heading', { name: 'PRIVACY POLICY' })).toBeVisible();
+});
+
+test('privacy policy page should be accessible from all sections in a dashboard when logged in', async ({ page }) => {
+  // Arrange
+  const user = {
+    name: "CS 4218 Test Account",
+    email: "cs4218@test.com",
+    password: "cs4218@test.com",
+  }
+  const dashboardSectionLinks = ['Create Category', 'Create Product', 'Products', 'Orders', 'Users'];
+
+  // Act
+  await page.getByRole('link', { name: 'Login' }).click();
+  await page.getByRole('textbox', { name: 'Enter Your Email' }).fill(user.email);
+  await page.getByRole('textbox', { name: 'Enter Your Password' }).fill(user.password);
+  await page.getByRole('button', { name: 'LOGIN' }).click();
+
+  for (const sectionLink of dashboardSectionLinks) {
+    await page.getByRole('button', { name: user.name }).click();
+    await page.getByRole('link', { name: 'Dashboard' }).click();
+    await page.getByRole('link', { name: sectionLink }).click();
+
+    await page.getByRole('link', { name: 'Privacy Policy' }).click();
+
+    // Assert
+    await expect(page.getByRole('heading', { name: 'PRIVACY POLICY' })).toBeVisible();
+  }  
+});
