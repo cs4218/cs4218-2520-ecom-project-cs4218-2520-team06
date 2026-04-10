514ec8398d7368b18ceb8c2b783b71bf33210b6a
GabrielCWT <77312579+GabrielCWT@users.noreply.github.com>
2026-04-10 14:14:20 +0800

    Add checkout load test

---
+++ non-functional-tests/load-tests/checkout-load.js
@@ -0,0 +1,57 @@
+import http from "k6/http";
+import { check } from "k6";
+import { baseOptions } from "./base-load.js";
+import { getRandomUserCredentials } from "./utils.js";
+import { BASE_URL } from "./scripts/constants.js";
+
+export const options = baseOptions;
+const MOCK_PAYMENT_PATH = "/api/v1/product/mock/payment";
+
+export default () => {
+  // Get random product
+  const productRes = http.get(`${BASE_URL}/api/v1/product/product-list/1`);
+  check(productRes, { "product list status is 200": (r) => r.status === 200 });
+  const products = productRes.json().products;
+  const product = products[Math.floor(Math.random() * products.length)];
+
+  // Login
+  const credentials = getRandomUserCredentials();
+  const loginPayload = JSON.stringify({
+    email: credentials.email,
+    password: credentials.password,
+  });
+
+  const loginParams = {
+    headers: { "Content-Type": "application/json" },
+  };
+
+  const loginRes = http.post(
+    `${BASE_URL}/api/v1/auth/login`,
+    loginPayload,
+    loginParams
+  );
+  check(loginRes, { "login status is 200": (r) => r.status === 200 });
+
+  const token = loginRes.json().token;
+  if (!token) return;
+
+  // Checkout
+  const checkoutParams = {
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: token,
+    },
+  };
+
+  const checkoutPayload = JSON.stringify({
+    cart: [product],
+  });
+
+  const res = http.post(
+    `${BASE_URL}${MOCK_PAYMENT_PATH}`,
+    checkoutPayload,
+    checkoutParams
+  );
+
+  check(res, { "mock payment status is 200": (r) => r.status === 200 });
+};
