90cad7bc133edc2f5d46db75972ab216a2206f95
C5hives <e1122945@u.nus.edu>
2026-04-12 11:58:24 +0800

    Change percentage of new users

--- non-functional-tests/soak-tests/soak-test.js
+++ non-functional-tests/soak-tests/soak-test.js
@@ -16,7 +16,7 @@ export const options = {
       startVUs: 0,
       stages: [
         { duration: "30m", target: 200 },
-        { duration: "20h", target: 200 },
+        { duration: "12h", target: 200 },
         { duration: "30m", target: 0 },
       ],
     },
@@ -52,8 +52,8 @@ export function setup() {
 export default function (data) {
     const userEmails = data.seededUserEmails;
 
-    // deterministic split of 80% returning users and 20% new users
-    const isSeeded = (__VU % 100) < 80;
+    // deterministic split of 90% returning users and 10% new users
+    const isSeeded = (__VU % 100) < 90;
 
     let email;
     if (isSeeded) {
